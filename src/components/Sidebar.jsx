16b45b2b0aad5665c5e77b8f7f974a2a1a3b1d8a
Dauda Adbulganiyu <daudakale@gmail.com>
2022-05-21 19:19:17 +0100

    sidebar closing handled, with the navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,8 +4,16 @@ import { SiShopware } from "react-icons/si";
 import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { links } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
+
 const Sidebar = () => {
-  const activeMenu = true;
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSideBar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
   const normalLink =
@@ -17,7 +25,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to={"/"}
-              onClick={() => {}}
+              onClick={handleCloseSideBar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware /> <span>Shoppy</span>
@@ -25,7 +33,9 @@ const Sidebar = () => {
             <TooltipComponent content={"Menu"} position={"BottomCenter"}>
               <button
                 type="button"
-                onClick={() => {}}
+                onClick={() =>
+                  setActiveMenu((prevActiveMenu) => !prevActiveMenu)
+                }
                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
@@ -42,7 +52,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSideBar}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
